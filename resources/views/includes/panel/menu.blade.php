f5b2d0bb3831488035b1846a427cdac25cf08347
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-03-07 21:36:13 -0500

    Pagina especialidades

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,22 +1,22 @@
 <h6 class="navbar-heading text-muted">Gestion</h6>
 <ul class="navbar-nav">
     <li class="nav-item  active ">
-      <a class="nav-link  active " href="./index.html">
+      <a class="nav-link  active " href="#">
         <i class="ni ni-tv-2 text-danger"></i> Panel de control
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link " href="./examples/icons.html">
+      <a class="nav-link " href="{{url('/especialidades')}}">
         <i class="ni ni-briefcase-24 text-blue"></i> Especialidades
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link " href="./examples/maps.html">
+      <a class="nav-link " href="#">
         <i class="fas fa-stethoscope text-info"></i> Médicos
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link " href="./examples/profile.html">
+      <a class="nav-link " href="#">
         <i class="fas fa-bed text-warning"></i> Pacientes
       </a>
     </li>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//rutas especialidades
+Route::get('/especialidades', [App\Http\Controllers\SpecialtyController::class, 'index']);
+Route::get('/especialidades/create', [App\Http\Controllers\SpecialtyController::class, 'create']);
+Route::get('/especialidades/{specialty}/edit', [App\Http\Controllers\SpecialtyController::class, 'edit']);
+Route::post('/especialidades', [App\Http\Controllers\SpecialtyController::class, 'senData']);
